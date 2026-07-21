b532f5d9dd7aa39c534654aa49c98ac6eea24e20
Gaston Brondani <69480995+GastonBrondani@users.noreply.github.com>
2026-07-21 20:39:46 -0300

    Rama gaston (#9)
    * Cambios en el sincro
    
    * Arreglando playload del pedido
    
    * fix id empresa en pago
    
    ---------
    
    Co-authored-by: GastonBrondani <gaston.brondani@gmail.com>

--- lib/repositories/pago_repository.dart
+++ lib/repositories/pago_repository.dart
@@ -49,6 +49,7 @@ class PagoRepository {
         payloadJson: jsonEncode({
           'client_uuid': localUuid,
           'idempotency_key': localUuid,
+          'id_empresa': 1,
           'legajo': legajo,
           'id_cuenta': idCuenta,
           'id_repartodia': idRepartoDia,
